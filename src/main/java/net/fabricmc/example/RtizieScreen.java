902d41a22408365e5a45ef98ef5471bc41b923cf
Alex <sasahrac@gmail.com>
2022-09-22 18:03:17 +0200

    MenuButton

---
+++ src/main/java/net/fabricmc/example/RtizieScreen.java
@@ -0,0 +1,31 @@
+package net.fabricmc.example;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.screen.ScreenTexts;
+import net.minecraft.text.Text;
+
+public class RtizieScreen extends Screen {
+    private final Screen parent;
+    private final GameOptions options;
+
+    public RtizieScreen(Screen parent, GameOptions options) {
+        super(Text.literal("Rtizie"));
+        this.parent = parent;
+        this.options = options;
+    }
+
+    Text flyingText(){
+        return Text.literal("Flying");
+    }
+
+    protected void init(){
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, this.height / 6 + 90, 100, 20, flyingText(),(flying) ->{
+            ExampleMod.LOGGER.info("FLYING");
+        }));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, this.height / 6 + 120, 100, 20, ScreenTexts.BACK,(flying) ->{
+            this.client.setScreen(parent);
+        }));
+    }
+}
--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,7 +1,9 @@
 package net.fabricmc.example.mixin;
 
 import net.fabricmc.example.ExampleMod;
+import net.fabricmc.example.RtizieScreen;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.world.SelectWorldScreen;
@@ -13,10 +15,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
+@Mixin(GameMenuScreen.class)
 public abstract class ExampleMixin extends Screen {
 
 	protected ExampleMixin(Text title) {
 		super(title);
+	}
+
+	@Inject(at = @At("HEAD"), method = "initWidgets")
+	private void initWidgets(CallbackInfo ci){
+		this.addDrawableChild(new ButtonWidget(10,10,90,10, Text.literal("Rtizie"), (menu) ->{
+			this.client.setScreen(new RtizieScreen(this,this.client.options));
+		}));
 	}
 }
